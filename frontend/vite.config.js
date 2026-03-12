cfeb79d98b84ca1eeac570b2af4f50da99ef5654
Developer-Mustafo <mustaforahimov30@gmail.com>
2026-03-12 20:36:06 +0500

    test 2.5 finally understood

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: [
+      'davomat-app.uz',
+      'www.davomat-app.uz',
+      'localhost',
+      '192.168.1.150'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
